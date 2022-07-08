d0e710fc482bec05df00d5c929a5765823fcca43
Андрій <Andrii.makarets@ukma.edu.ua>
2022-07-08 22:05:41 +0300

    Selenium

---
+++ src/test/java/Selenium/SeleniumTest.java
@@ -0,0 +1,81 @@
+package Selenium;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.openqa.selenium.TimeoutException;
+
+
+public class SeleniumTest {
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+    @Test
+    void testCheckboxAndRadio() {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+        WebElement checkbox2 = driver.findElement(By.id("my-check-2"));
+        checkbox2.click();
+        assertThat(checkbox2.isSelected()).isTrue();
+
+        WebElement radio2 = driver.findElement(By.id("my-radio-2"));
+        radio2.click();
+        assertThat(radio2.isSelected()).isTrue();
+    }
+
+    @Test
+    void testPageLoadTimeout() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofMillis(1));
+
+        assertThatThrownBy(() -> driver
+                .get("https://bonigarcia.dev/selenium-webdriver-java/"))
+                .isInstanceOf(TimeoutException.class);
+    }
+
+}
